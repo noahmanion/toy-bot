bbd7ba52353e8c62b49688757c4379267c42b82e
Noah Manion <noah.manion@gmail.com>
2016-08-31 21:22:41 -0500

    updated the bot to speak

--- index.js
+++ index.js
@@ -29,3 +29,38 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+app.post('/webhook/', function (req, res) {
+	messaging_events = req.body.entry[0].messaging
+	for (i = 0; i < messaging_events.length; i++) {
+		event = req.body.entry[0].messaging[i]
+		sender = event.sender.id
+		if (event.message && event.message.text) {
+			text = event.message.text
+			sendTextMessage(sender, "Text recevieved, echo: " + text.substring(0,200))
+		}
+	}
+	res.sendStatus(200)
+})
+var token = "EAADamq4J1aMBAKQ9V3kCJhR442jK9iruICSrXvOTPdz3ITXG6X80wFX7ll6V2q0Dc0q6JkuqMD9km1JcCGbQ87Fe3t1HsZBZAMJd9qOcqKFJG14U0CRaPw0FnsoMZBheocV3kv0AG4ZB5q6v2n9ImLEGZAg4272AyZBCZCjuGm9VQZDZD"
+
+functio sendTextMessage(sender, text) {
+	messageData = {
+		text:text
+	}
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'POST'.
+		json: {
+			recipient: {id:sender},
+			message: messageData
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}
